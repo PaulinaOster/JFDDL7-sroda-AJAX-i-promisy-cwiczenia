b6de3c529a7e9228ccfb20c711f886e912f5c55c
Paulina Oster <paulina.oster@gmail.com>
2019-03-27 11:24:25 +0100

    loadUsers function (fetching random users)

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 class App {
-    constructor (selector) {
+    constructor(selector) {
         this.container = document.querySelector(selector) || document.body;
         this.numberOfUsers = 10;
         this.genderOfUsers = ''; //this.means both genders
         this.searchTerm = '';
         this.users = null;
         this.focusedElement = null;
+        this.isLoading = false;
+        this.isError = false;
 
         this.render();
     }
 
-    render () {
-        this.container.innerHTML = '';
+    loadUsers() {
+        if (this.isLoading) return;
 
-        this.renderInput('number','numberOfUsers');
-        this.renderInput('text','genderOfUsers');
-        this.renderButton('Załaduj', () => {})
+        this.isLoading = true;
+        this.isError = false;
+        
+        fetch(`https://randomuser.me/api/?results=${this.numberOfUsers}&gender=${this.genderOfUsers}`)
+            .then(response => response.json())
+            .then(data => { this.users = data.results; console.log(this) })
+            .catch(() => this.isError = true)
+            .finally(() => this.isLoading = false)
     }
 
-    renderInput (type, propertyName) {
+    render() {
+        this.container.innerHTML = '';
+
+        this.renderInput('number', 'numberOfUsers');
+        this.renderInput('text', 'genderOfUsers');
+        this.renderButton('Załaduj', this.loadUsers.bind(this)) // funkcja onClick jest przekazana do funkcji bez kontekstu zatem trzeba dodać kontekst poprzez .bind(this)
+    }
+
+    renderInput(type, propertyName) {
         const input = document.createElement('input');
-        input.setAttribute('type',type);
+        input.setAttribute('type', type);
         input.value = this[propertyName];
         input.addEventListener(
             'input',
@@ -34,7 +49,7 @@ class App {
         if (this.focusedElement === propertyName) input.focus();
     }
 
-    renderButton (label,onClick) {
+    renderButton(label, onClick) {
         const button = document.createElement('button');
         button.innerText = label;
         button.addEventListener(
@@ -44,13 +59,3 @@ class App {
         this.container.appendChild(button);
     }
 }
-
-
-fetch('https://randomuser.me/api/?results=100&nat=dk&gender=female')
-    .then(response => response.json())
-    .then(data => {
-        for (let i = 0; i < data.results.length; i++) {
-            console.log(data.results[i].email)
-        }
-    }
-    )
